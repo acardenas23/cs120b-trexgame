07e9487d9a37c3daae5f931b6699a5564d28a0ff
Ana Cardenas Beltran <acard079@ucr.edu>
2021-06-06 16:34:52 -0700

    final project

--- finalProject/source/main.c
+++ finalProject/source/main.c
@@ -1,7 +1,7 @@
 /*	Author: acard079
  *  Partner(s) Name: 
  *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab #11  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -26,6 +26,7 @@ int main(void){
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	DDRD = 0xFF; PORTD = 0x00;//Set PORTD to output since we are using OC1A Pin(PD5)
+	
 	TCCR1A |= 1 << WGM11 | 1 << COM1A1 | 1 << COM1A0; //shift operator to set bits, setting COM1A and COM10 to 1 sets inverting mode (pg176)
 	TCCR1B |= 1 << WGM12 | 1 << WGM13 | 1 << CS10;
 	ICR1 = 19999;
@@ -34,19 +35,17 @@ int main(void){
 
 	while(1){
 		x = ADC;
-	
 		//code to optain brightness level when there is cactus on screen (comment out)
 		/*
 		unsigned char lower8 = (char)x;
 		
-		PORTB = lower8; //value obtained-> 0111 1111 = 0x7F
+		PORTB = lower8;
 		*/
 		cactusval = 0x9A; //brightness level when there is cactus
 
 		if(x <= cactusval){
 			//there is cactus
-			PORTB = 0x01;
-			
+			PORTB = 0x01;			
 			OCR1A = ICR1 - 2000;
 			_delay_ms(100); 
 		}else{
@@ -55,8 +54,6 @@ int main(void){
 			OCR1A = ICR1 - 1550; //position 0
 			_delay_ms(50);
 		}
-
-			
 	}
 	return 1;
 }
